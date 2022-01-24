a8312af96cc72a0dacea423c2536c9e31043e746
gello77777 <gello77777@yandex.ru>
2022-01-24 14:15:32 +0700

    Initial commit

---
+++ src/main/java/com/example/springboot/model/User.java
@@ -0,0 +1,25 @@
+package com.example.springboot.model;
+
+
+import lombok.Data;
+
+import javax.persistence.*;
+@Data
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "age")
+    private int age;
+
+    @Column(name = "city")
+    private String city;
+
+}
